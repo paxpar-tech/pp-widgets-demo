f85fd4569d6917258255eac7d60d4f0d96fe7a46
Philippe ENTZMANN <philippe@arundo.tech>
2023-11-29 23:25:49 +0100

    fix: initial commit

---
+++ pages/nav.vue
@@ -0,0 +1,23 @@
+<script setup>
+const links = [{
+  label: 'Home',
+  icon: 'i-heroicons-home',
+  to: '/'
+}, {
+  label: 'Navigation',
+  icon: 'i-heroicons-square-3-stack-3d'
+}, {
+  label: 'Breadcrumb',
+  icon: 'i-heroicons-link'
+}]
+</script>
+
+<template>
+  <UBreadcrumb :links="links" />
+
+  <UButton color="primary">Documents</UButton>
+  <UButton color="primary" variant="ghost">Craftforms</UButton>
+  <UButton color="primary" variant="ghost">Checklist</UButton>
+
+</template>
+
---
+++ pages/pricing.vue
@@ -0,0 +1,8 @@
+<template>
+    <UPricingGrid :compact="false">
+        <UPricingCard title="Free" description="Get started for free in development." />
+        <UPricingCard title="Solo" description="For bootstrappers and indie hackers." price="$199" scale highlight />
+        <UPricingCard title="Team" description="Unlimited license for growing teams." price="$699" />
+    </UPricingGrid>
+</template>
+  
