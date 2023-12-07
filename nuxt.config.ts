f4fcc47b15ac08b19af34571f50169b7b1547fc7
Philippe ENTZMANN <philippe.entzmann@gmail.com>
2023-12-07 11:48:32 +0000

    fix: wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,39 @@
+import { logger } from "@nuxt/kit";
+import { fileURLToPath } from "url";
+import { resolve } from "pathe";
+import { version } from "./package.json";
+//import ModuleTransformPDF from "./modules/transformpdf/module";
+
+logger.success(`Using pp-widgets-demo v${version}`);
+
+const themeDir = fileURLToPath(new URL("./", import.meta.url));
+const resolveThemeDir = (path: string) => resolve(themeDir, path);
+
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  components: [
+    //{
+    //  // TODO: useful ?
+    //  prefix: "",
+    //  path: resolveThemeDir("./components/content"),
+    //  global: true,
+    //},
+    //{
+    //  // publish components that overload existing nuxt/content/docus components
+    //  prefix: "",
+    //  path: resolveThemeDir("./components/overload"),
+    //  global: true,
+    //},
+    {
+      // all components are published with ppw- prefix
+      prefix: "ppw",
+      path: resolveThemeDir("./components/ppw"),
+      global: true,
+    },
+  ],
+
+
   extends: ['@nuxt/ui-pro'],
   modules: [
     '@nuxt/content',
